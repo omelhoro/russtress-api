776832272dbce4ae8df3e8b4fdd6bcddb87864a8
omelhoro <igor.fischer154@gmail.com>
2015-02-07 17:00:37 +0100

    Pep8 format and explicit class calling

--- webapi_blue.py
+++ webapi_blue.py
@@ -1,37 +1,36 @@
-from flask import Flask,Blueprint, render_template, abort,request
-from jinja2 import TemplateNotFound
+from flask import Flask, Blueprint, render_template, request
 from setstress import setup_stress
 import json
-import sys
-reload(sys)
-sys.setdefaultencoding("utf8")
 
 
-def setup_rs(cl=Blueprint):
-    if cl==Flask:
-        simple_page = cl(__name__,static_folder='web',template_folder='web')
-        homeroute="/" 
+def setup_rs(cl):
+    if cl == Flask:
+        simple_page = Flask(__name__)
+        homeroute = "/"
     else:
-        simple_page = cl("rust",__name__,static_folder='web',template_folder='web')
-        homeroute="/rustress"
+        simple_page = Blueprint("rust")
+        homeroute = "/rustress"
+
     @simple_page.route(homeroute)
     def rustress():
         return render_template("index.html")
 
     try:
-        set_stress,pm=setup_stress("./rustress/dict_data")
-    except:
+        set_stress, pm = setup_stress("./rustress/dict_data")
+    except FileNotFoundError:
         try:
-            set_stress,pm=setup_stress("./dict_data")
-        except:
-            set_stress,pm=setup_stress("./mysite/rustress/dict_data")
+            set_stress, pm = setup_stress("./dict_data")
+        except FileNotFoundError:
+            set_stress, pm = setup_stress("./mysite/rustress/dict_data")
 
     @simple_page.route("/stress")
     def stress():
-        return json.dumps({k:list(set_stress(pm,k.decode("utf8"))) for k,v in request.args.items()})
+        return json.dumps({k: list(set_stress(pm, k)) for k, v in request.args.items()})
+
     return simple_page
 
-if __name__ == "__main__": 
-    app=setup_rs(Flask)
+
+if __name__ == "__main__":
+    app = setup_rs(Flask)
     app.debug = True
     app.run()
